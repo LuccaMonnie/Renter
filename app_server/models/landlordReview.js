28ecd3003f483a49c8bd1f6a8678db3e9e5f9378
Nicole Cahlander <ncahlander@pugetsound.edu>
2018-02-07 10:50:02 -0800

    data schema

--- app_server/models/landlordReview.js
+++ app_server/models/landlordReview.js
@@ -3,5 +3,9 @@
 var mongoose = require('mongoose');
 
 var landlordReviewSchema = new mongoose.Schema({
+  //responsive, easy to get in contact with
+  communication: Number,
+  fixProblem: Number, //how quickly did they respond with help
+  //more??
 
 });
--- app_server/models/tenantReview.js
+++ app_server/models/tenantReview.js
@@ -4,7 +4,32 @@ var mongoose = require('mongoose');
 //construct a schema
 var tenantReviewSchema = new mongoose.Schema({
   account : String, //make this specific to person's account?
-  rating: {type: Number, "default": 0},
-  notes: String //should we have this be a list [String] that we can parse to
+  rating: {type: Number, "default": 0, required: true}, //SET REQUIRED, validation error, create way to see on the webpage
+  notice: Boolean, // notice given
+  rentLeft: Boolean, //is there still unpaid rent
+  latePayment: Boolean, //did they have late rent?
+  lateThreeDay: Boolean,
+  latePaymentNum: Number,
+  lateThreeDayNum: Number,
+  damages: Boolean,
+  damageDescription: String,
+  complaintsFiled: Boolean, //legality of reporting what the complaints were?
+  complaintDescription: String,
+  undocumentedHousemates: Boolean, //undocumented tenants or pets
+  coTenants: Boolean,
+  rentalStatus: Boolean,//CHECK?? on a written rental agreement meaning
+  removed: Boolean, //asked to vacate?
+  rentAgain: Boolean,
+  pets: Boolean,
+  petType: String,
+  deposit: Boolean,
+  moveOutDate: String, // calendar dropdown
+  rentalAmount: Number,
+  lease: Boolean,
+
+  notes: String, //should we have this be a list [String] that we can parse to
   //group people together
+
+
+
 });
