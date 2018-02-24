9a46eb2e31c4b70e9ae786468d56594900aaef7e
Nicole Cahlander <ncahlander@pugetsound.edu>
2018-02-23 17:27:19 -0800

    update

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var ctrlAbout = require('../controllers/about')
+//add more like this if we have more pages ^
 
+//routes serve as mapping service
 
+//pg 84
 
- /* GET home page */
+/* GET home page */
 router.get('/', ctrlMain.index);
+
+/* ABOUT pages*/
+//define routes and map them to controller functions
+router.get('/about', ctrlAbout.aboutInfo);
 
 module.exports = router;
